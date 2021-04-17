72d1e2d9b5a737018ed200aae4c04473f508e4e5
Alex <flowalex@pop-os.localdomain>
2021-04-17 10:01:52 -0500

    Added Hello World from lession 13

---
+++ non_rails/hello_world.rb
@@ -0,0 +1,16 @@
+# Different ways to say hello Section 1 Lession 13
+# Just printing out hello world
+puts 'Hello World'
+p 'Hello World'
+print "Hello World\n"
+# print "Hello World" #Print doesn't add a new line at the end
+# Creating a variable and then printing the out the output of the variable
+greeting = 'Hello World'
+
+puts greeting
+
+def say_hello(thing_to_say)
+  puts thing_to_say
+end
+
+say_hello 'Hello Alex'
--- non_rails/quiz1.rb
+++ non_rails/quiz1.rb
@@ -1,35 +1,19 @@
-address = [1, 2, 3,4, 5, 6, 7, 8, 9]
-p address
-=begin
- Question 3:
-Let's say I have a sequence of numbers -> [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-I can use an address to reference this sequence of numbers so I can retrieve them anytime I want..for example I can say address = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-If after that I referenced address, it will return the whole sequence of numbers back to me
-
-I can do more, if I say address[0], it returns the number 1. The value within the [ ] (square brackets) is called the index. Indexing starts at 0 (instead of 1), therefore the first item in my sequence (which is 1) is at index 0. Similarly ->
-
-if I say address[1], it returns the number 2
-
-if I say address[8], it returns the number 9
-
-What if I say address[5], what does it return?
-
-=end
-p address[5]
-=begin
-Let's say I have the same sequence and address as the prior question below:
-
 address = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+p address
+#  Question 3:
+# Let's say I have a sequence of numbers -> [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# I can use an address to reference this sequence of numbers so I can retrieve them anytime I want..for example I can say address = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# If after that I referenced address, it will return the whole sequence of numbers back to me
+# I can do more, if I say address[0], it returns the number 1. The value within the [ ] (square brackets) is called the index. Indexing starts at 0 (instead of 1), therefore the first item in my sequence (which is 1) is at index 0. Similarly ->
+# if I say address[1], it returns the number 2
+# if I say address[8], it returns the number 9
+# What if I say address[5], what does it return?
 
-If I said address.reverse! (with an exclamation mark), it would have returned [9, 8, 7, 6, 5, 4, 3, 2, 1]. I didn't say it though, instead I ended up saying ->
-
-new_address = address.reverse!
-
-What will new_address be?
-=end
+p address[5]
+# Let's say I have the same sequence and address as the prior question below:
+# address = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# If I said address.reverse! (with an exclamation mark), it would have returned [9, 8, 7, 6, 5, 4, 3, 2, 1]. I didn't say it though, instead I ended up saying ->
+# new_address = address.reverse!
+# What will new_address be?
 new_address = address.reverse! # you could also just do p address.reverse
 p new_address
-
-
