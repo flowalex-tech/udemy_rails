d6616a0c518016134ae4833744a2743ff40d6199
Alex <flowalex@vivaldi.net>
2021-04-25 13:25:46 -0500

    added comparision operators

---
+++ non_rails/1_22_comparision.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# Section 1 Lession 22 Brief look at comparison operators
+
+# == equals
+#  != not equal
+#  > Greater than
+# < less than
+# >= Greater than or equal to
+# <= less than or euqal to
+# .eql? Comparing types
